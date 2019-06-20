8178a086d57944a81f7e2fe2f5196e825ad6e8a0
AgonAh <aa40810@ubt-uni.net>
2019-06-20 20:43:38 +0200

    Fixed a ridiculous typo

--- src/GUI/View/POSForm.java
+++ src/GUI/View/POSForm.java
@@ -470,7 +470,7 @@ public class POSForm extends javax.swing.JInternalFrame {
         try {
             Connection conn = DriverManager.getConnection("jdbc:sqlserver://localhost:1433", "sa", "sa");
             java.sql.Statement stmt = conn.createStatement();
-            String exec = "use PharmacyManagement select BrandName,GenericName,s.ExpiryDate,Quantity,s.supplydate,Price,m.medicineId from stock s inner join medicine m on s.MedicineID=m.MedicineID where m.BrandName like '%" + name + "%' && s.statusId=1 order by s.ExpiryDate asc";
+            String exec = "use PharmacyManagement select BrandName,GenericName,s.ExpiryDate,Quantity,s.supplydate,Price,m.medicineId from stock s inner join medicine m on s.MedicineID=m.MedicineID where m.BrandName like '%" + name + "%' and s.statusId=1 order by s.ExpiryDate asc";
             ResultSet rset = stmt.executeQuery(exec);
             model.setRowCount(0);
             while (rset.next()) {
